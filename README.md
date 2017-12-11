93f8bd86646c3b16ba3a6555c66de7aa63ed9042
ml250271 <lukicmilica@hotmail.com>
2017-12-11 12:13:55 +0100

    check controlNum

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 JMBG
 ---
+/*var day1;
+var day2;
+var month1;
+var month2;
+var year1;
+var year2;
+var year3;
+var region1;
+var region2;
+var uniqueW1;
+var uniqueW2;
+var uniqueW3;
+var uniqueM1;
+var uniqueM2;
+var uniqueM3;
+var control;*/
+/*function isValidDate(value) {
+    var dateWrapper = new Date(value);
+    return !isNaN(dateWrapper.getDate());
+}*/
+
+/*var jmbgInput = document.getElementById("jmbgInput");
+var ValueJMBG = jmbgInput.value;*/
--- js/JMBG.js
+++ js/JMBG.js
@@ -1,46 +1,71 @@
-/*var day1;
-var day2;
-var month1;
-var month2;
-var year1;
-var year2;
-var year3;
-var region1;
-var region2;
-var uniqueW1;
-var uniqueW2;
-var uniqueW3;
-var uniqueM1;
-var uniqueM2;
-var uniqueM3;
-var control;*/
+
 /*function isValidDate(value) {
     var dateWrapper = new Date(value);
     return !isNaN(dateWrapper.getDate());
 }*/
 
+/*var jmbgInput = document.getElementById("jmbgInput");
+var ValueJMBG = jmbgInput.value;*/
+var jmbgInput = document.getElementById("jmbgInput");
+function getNumbers() {
 var jmbgInput = document.getElementById("jmbgInput");
 var ValueJMBG = jmbgInput.value;
-
-function getNumbers() {
 var array = ValueJMBG.split("");
 console.log(array);
+    if (isThirteen) {
+        var a1 = array[0];
+        var a2 = array[1];
+        var a3 = array[2];
+        var a4 = array[3];
+        var a5 = array[4];
+        var a6 = array[5];
+        var a7 = array[6];
+        var a8= array[7];
+        var a9 = array[8];
+        var a10 = array[9];
+        var a11 = array[10];
+        var a12 = array[11];
+        var a13 = array[12];
+        var k;
+         
+        s = 7*a1 + 6*a2 + 5*a3 + 4*a4 + 3*a5 + 2*a6 + 7*a7 + 6*a8 + 5*a9 + 4*a10 + 3*a11 + 2*a12 ;         
+        k = 11 - s%11;
+        console.log(s,k);
+        
+        if ( a13 == k) {
+          document.getElementById("rezultat").innerHTML = "Kontrolni broj je validan." 
+            
+        }
+        else {
+          document.getElementById("rezultat").innerHTML = "Kontrolni broj nije validan."   
+        }
+        
+        
+        
+    }
+    else {
+        document.getElementById("rezultat").innerHTML = "JMBG treba da ima 13 cifara."
+    }
 }
 
 
+
+
+
 function isThirteen() {
-    
-console.log(ValueJMBG);
+    var ValueJMBG = jmbgInput.value;
     if ( ValueJMBG.length === 13) {
         console.log("13 cifara");
             
             } 
-    else {
-        document.getElementById("rezultat").innerHTML = "JMBG treba da ima 13 cifara."
-        
-    }
+
+    console.log(ValueJMBG);
     
  }
+
+
+
+
 
 
 
